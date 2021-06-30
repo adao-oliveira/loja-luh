6c19b964e457f6b461f81ade1569836ffc45934b
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-06-30 15:50:00 -0300

    ajustes no desing do login

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import {useState, useContext, useEffect} from 'react'
-import {DataContext} from '../store/GlobalState'
-import {postData} from '../utils/fetchData'
+import { useState, useContext, useEffect } from 'react'
+import { DataContext } from '../store/GlobalState'
+import { postData } from '../utils/fetchData'
 import Cookie from 'js-cookie'
 import { useRouter } from 'next/router'
 
@@ -11,29 +11,31 @@ const Login = () => {
   const [userData, setUserData] = useState(initialState)
   const { email, password } = userData
 
-  const {state, dispatch} = useContext(DataContext)
+  const { state, dispatch } = useContext(DataContext)
   const { auth } = state
 
   const router = useRouter()
 
   const handleChangeInput = e => {
-    const {name, value} = e.target
-    setUserData({...userData, [name]:value})
+    const { name, value } = e.target
+    setUserData({ ...userData, [name]: value })
     dispatch({ type: 'NOTIFY', payload: {} })
   }
 
   const handleSubmit = async e => {
     e.preventDefault()
-    dispatch({ type: 'NOTIFY', payload: {loading: true} })
+    dispatch({ type: 'NOTIFY', payload: { loading: true } })
     const res = await postData('auth/login', userData)
-    
-    if(res.err) return dispatch({ type: 'NOTIFY', payload: {error: res.err} })
-    dispatch({ type: 'NOTIFY', payload: {success: res.msg} })
 
-    dispatch({ type: 'AUTH', payload: {
-      token: res.access_token,
-      user: res.user
-    }})
+    if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+    dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+
+    dispatch({
+      type: 'AUTH', payload: {
+        token: res.access_token,
+        user: res.user
+      }
+    })
 
     Cookie.set('refreshtoken', res.refresh_token, {
       path: 'api/auth/accessToken',
@@ -44,36 +46,41 @@ const Login = () => {
   }
 
   useEffect(() => {
-    if(Object.keys(auth).length !== 0) router.push("/")
+    if (Object.keys(auth).length !== 0) router.push("/")
   }, [auth])
 
-    return(
-      <div>
-        <Head>
-          <title>Pagina de Login</title>
-        </Head>
+  return (
+    <div>
+      <Head>
+        <title>Pagina de Login</title>
+      </Head>
 
-        <form className="mx-auto my-4" style={{maxWidth: '500px'}} onSubmit={handleSubmit}>
-          <div className="form-group mt-48">
-            <label htmlFor="exampleInputEmail1">E-mail</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            name="email" value={email} onChange={handleChangeInput} />
-            <small id="emailHelp" className="form-text text-muted"></small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Senha</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"
-            name="password" value={password} onChange={handleChangeInput} />
-          </div>
-          
-          <button type="submit" className="btn btn-dark w-100">Login</button>
+      <main class="container mt-48 mb-32" style={{ maxWidth: '600px' }}>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div class="input-field">
+            <input type="email" id="exampleInputEmail1" aria-describedby="emailHelp"
+              name="email" value={email} onChange={handleChangeInput}  placeholder="E-mail" />
 
+              <div class="underline"></div>
+
+          </div>
+          <div class="input-field">
+            <input type="password" id="exampleInputPassword1"
+              name="password" value={password} onChange={handleChangeInput} placeholder="Senha" />
+
+              <div class="underline"></div>
+              
+          </div>
+
+          <input type="submit" value="Login" />
           <p className="my-2">
-            Ainda não tem conta? <Link href="/registrar"><a style={{color: 'crimson'}}>Registrar agora</a></Link>
+            Ainda não tem conta? <Link href="/registrar"><a style={{ color: 'crimson' }}>Registrar agora</a></Link>
           </p>
         </form>
-      </div>
-    )
-  }
-  
-  export default Login
+      </main>
+    </div>
+  )
+}
+
+export default Login
