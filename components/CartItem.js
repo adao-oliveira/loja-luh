6fd2748ae6ca24165e64b9999c3c523e20f51f30
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-07-01 23:57:51 -0300

    ajustes nos detalhes do site

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { decrease, increase } from '../store/Actions'
 
-const CartItem = ({item, dispatch, carrinho}) => {
+const CartItem = ({item, dispatch, cart}) => {
     return (
         <tr>
             <td style={{width: '100px', overflow: 'hidden'}}>
@@ -11,22 +11,30 @@ const CartItem = ({item, dispatch, carrinho}) => {
             </td>
 
             <td style={{minWidth: '200px'}} className="w-50 align-middle" >
-                <h5 className="text-capitalize text-dark">
+                <h5 className="text-capitalize text-secondary">
                     <Link href={`/product/${item._id}`}>
                         <a>{item.title}</a>
                     </Link>
                 </h5>
-                <h6 className="text-danger">R${item.quantity * item.price}</h6>
+
+                <h6 className="text-danger">${item.quantity * item.price}</h6>
+                {
+                    item.inStock > 0
+                    ? <p className="mb-1 text-danger">In Stock: {item.inStock}</p>
+                    : <p className="mb-1 text-danger">Out Stock</p>
+                }
             </td>
 
             <td className="align-middle" style={{minWidth: '150px'}}>
-                <button className="btn btn-outline-dark"
-                onClick={ () => dispatch(decrease(carrinho, item._id)) } 
+                <button className="btn btn-outline-secondary"
+                onClick={ () => dispatch(decrease(cart, item._id)) } 
                 disabled={item.quantity === 1 ? true : false} > - </button>
 
                 <span className="px-3">{item.quantity}</span>
 
-                <button className="btn btn-outline-dark" onClick={ () => dispatch(increase(carrinho, item._id)) } > + </button>
+                <button className="btn btn-outline-secondary"
+                onClick={ () => dispatch(increase(cart, item._id)) }
+                disabled={item.quantity === item.inStock ? true : false} > + </button>
             </td>
 
             <td className="align-middle" style={{minWidth: '50px', cursor: 'pointer'}}>
@@ -34,7 +42,7 @@ const CartItem = ({item, dispatch, carrinho}) => {
                 style={{fontSize: '18px'}} data-toggle="modal" data-target="#exampleModal"
                 onClick={() => dispatch({
                     type: 'ADD_MODAL',
-                    payload: [{ data: carrinho, id: item._id, title: item.title, type: 'ADD_CART' }]
+                    payload: [{ data: cart, id: item._id, title: item.title, type: 'ADD_CART' }]
                 })} ></i>
             </td>
         </tr>
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -12,57 +12,68 @@ const DetailProduct = (props) => {
     const { carrinho } = state
 
     const isActive = (index) => {
-        if (tab === index) return " active";
+        if(tab === index) return " active";
         return ""
     }
 
-    return (
-        <div className="detail_page w-full max-w-lg container mt-48 mb-32">
+    return(
+        <div className="row detail_page mt-48">
             <Head>
                 <title>Detalhes do Produto</title>
             </Head>
-            <div className="grid justify-items-center">
-                <div className="flex flex-wrap">
-                    <div class="w-full md:w-1/2 col-md-6 px-3 mb-3 md:mb-0">
-                        <img src={product.images[tab].url} alt={product.images[tab].url} className="d-block img-thumbnail rounded w-100" style={{ height: '350px' }} />
-                        <div className="row mx-0" style={{ cursor: 'pointer' }} >
-                            {product.images.map((img, index) => (
-                                <img key={index} src={img.url} alt={img.url}
-                                    className={`img-thumbnail rounded ${isActive(index)}`}
-                                    style={{ height: '80px', width: '20%' }}
-                                    onClick={() => setTab(index)} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className="flex flex-wrap">
-                        <div class="w-full md:w-1/2 col-md-12 px-3 md:mb-0">
-                            <h1 className="text-uppercase font-light text-3xl">{product.title}</h1>
-                            <h5 className="text-danger">R${product.price}</h5>
-                            <div className="my-2">{product.description}</div>
-                            <div className="my-2">
-                                {product.content}
-                            </div>
 
-                            <div className="grid justify-items-left">
-                                <button type="button" className="btn btn-danger d-block my-2 px-4 w-50" onClick={() => dispatch(addToCart(product, carrinho))} >
-                                    Comprar
-                                </button>
-                            </div>
+            <div className="col-md-6">
+                <img src={ product.images[tab].url } alt={ product.images[tab].url }
+                className="d-block img-thumbnail rounded mt-4 w-100"
+                style={{height: '350px'}} />
 
-                        </div>
-                    </div>
+                <div className="row mx-0" style={{cursor: 'pointer'}} >
+
+                    {product.images.map((img, index) => (
+                        <img key={index} src={img.url} alt={img.url}
+                        className={`img-thumbnail rounded ${isActive(index)}`}
+                        style={{height: '80px', width: '20%'}}
+                        onClick={() => setTab(index)} />
+                    ))}
+
                 </div>
+            </div>
+
+            <div className="col-md-6 mt-3">
+                <h2 className="text-uppercase">{product.title}</h2>
+                <h5 className="text-danger">R${product.price}</h5>
+
+                <div className="row mx-0 d-flex justify-content-between">
+                    {
+                        product.inStock > 0
+                        ? <h6 className="text-danger">Em estoque: {product.inStock}</h6>
+                        : <h6 className="text-danger">Fora de estoque</h6>
+                    }
+
+                    <h6 className="text-danger">Vendido: {product.sold}</h6>
+                </div>
+
+                <div className="my-2">{product.description}</div>
+                <div className="my-2">
+                    {product.content}
+                </div>
+
+                <button type="button" className="btn btn-danger d-block my-3 px-5"
+                onClick={() => dispatch(addToCart(product, carrinho))} >
+                    Comprar
+                </button>
+
             </div>
         </div>
     )
 }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({params: {id}}) {
 
     const res = await getData(`product/${id}`)
     // server side rendering
     return {
-        props: { product: res.product }, // will be passed to the page component as props
+      props: { product: res.product }, // will be passed to the page component as props
     }
 }
 
