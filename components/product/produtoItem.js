e71e443c145600351f4763b05e3dda29fb68224e
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-07-01 23:53:46 -0300

    ajustes nos detalhes do site

--- components/product/produtoItem.js
+++ components/product/produtoItem.js
@@ -3,18 +3,21 @@ import { useContext } from 'react'
 import { DataContext } from '../../store/GlobalState'
 import { addToCart } from '../../store/Actions'
 
-const produtoItem = ({ product, handleCheck }) => {
+const produtoItem = ({product, handleCheck}) => {
     const { state, dispatch } = useContext(DataContext)
-    const { carrinho, auth } = state
+    const { cart, auth } = state
 
     const userLink = () => {
-        return (
+        return(
             <>
                 <Link href={`product/${product._id}`}>
                     <a className="btn btn-success"
-                        style={{ marginRight: '5px', flex: 1 }}>Visualizar</a>
+                    style={{marginRight: '5px', flex: 1}}>Visualizar</a>
                 </Link>
-                <button className="btn btn-danger" style={{ marginLeft: '5px', flex: 1 }} onClick={() => dispatch(addToCart(product, carrinho))} >
+                <button className="btn btn-danger"
+                style={{marginLeft: '5px', flex: 1}}
+                disabled={product.inStock === 0 ? true : false} 
+                onClick={() => dispatch(addToCart(product, cart))} >
                     Comprar
                 </button>
             </>
@@ -22,36 +25,36 @@ const produtoItem = ({ product, handleCheck }) => {
     }
 
     const adminLink = () => {
-        return (
+        return(
             <>
-                <Link href={`criarProduto/${product._id}`}>
-                    <a className="btn btn-dark"
-                        style={{ marginRight: '5px', flex: 1 }}>Editar</a>
+                <Link href={`create/${product._id}`}>
+                    <a className="btn btn-info"
+                    style={{marginRight: '5px', flex: 1}}>Edit</a>
                 </Link>
                 <button className="btn btn-danger"
-                    style={{ marginLeft: '5px', flex: 1 }}
-                    data-toggle="modal" data-target="#exampleModal"
-                    onClick={() => dispatch({
-                        type: 'ADD_MODAL',
-                        payload: [{
-                            data: '', id: product._id,
-                            title: product.title, type: 'DELETE_PRODUCT'
-                        }]
-                    })} >
-                    Deletar
+                style={{marginLeft: '5px', flex: 1}}
+                data-toggle="modal" data-target="#exampleModal"
+                onClick={() => dispatch({
+                    type: 'ADD_MODAL',
+                    payload: [{ 
+                        data: '', id: product._id, 
+                        title: product.title, type: 'DELETE_PRODUCT' 
+                    }]
+                })} >
+                    Delete
                 </button>
             </>
         )
     }
 
-    return (
+    return(
         <div className="card" style={{ width: '18rem' }}>
             {
                 auth.user && auth.user.role === 'admin' &&
                 <input type="checkbox" checked={product.checked}
-                    className="position-absolute"
-                    style={{ height: '20px', width: '20px' }}
-                    onChange={() => handleCheck(product._id)} />
+                className="position-absolute"
+                style={{height: '20px', width: '20px'}}
+                onChange={() => handleCheck(product._id)} />
             }
             <img className="card-img-top" src={product.images[0].url} alt={product.images[0].url} />
             <div className="card-body">
@@ -60,13 +63,18 @@ const produtoItem = ({ product, handleCheck }) => {
                 </h5>
 
                 <div className="row justify-content-between mx-0">
-                    <h6 className="text-danger">R${product.price}</h6>
+                    <h6 className="text-danger">${product.price}</h6>
+                    {
+                        product.inStock > 0
+                        ? <h6 className="text-danger">In Stock: {product.inStock}</h6>
+                        : <h6 className="text-danger">Out Stock</h6>
+                    }
                 </div>
 
                 <p className="card-text" title={product.description}>
                     {product.description}
                 </p>
-
+                    
                 <div className="row justify-content-between mx-0">
                     {!auth.user || auth.user.role !== "admin" ? userLink() : adminLink()}
                 </div>
--- pages/carrinho.js
+++ pages/carrinho.js
@@ -38,6 +38,13 @@ const Cart = () => {
       const updateCart = async () => {
         for (const item of cartLocal) {
           const res = await getData(`product/${item._id}`)
+          const { _id, title, images, price, inStock, sold } = res.product
+          if (inStock > 0) {
+            newArr.push({
+              _id, title, images, price, inStock, sold,
+              quantity: item.quantity > inStock ? 1 : item.quantity
+            })
+          }
         }
 
         dispatch({ type: 'ADD_CART', payload: newArr })
@@ -54,7 +61,7 @@ const Cart = () => {
     let newCart = [];
     for (const item of carrinho) {
       const res = await getData(`product/${item._id}`)
-      if (res.item.quantity >= 0) {
+      if (res.product.inStock - item.quantity >= 0) {
         newCart.push(item)
       }
     }
