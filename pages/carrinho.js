998f12fdcd02144b8071a5471ec053e8080b83f5
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-07-02 01:35:52 -0300

    ajustes nos detalhes do site

--- pages/carrinho.js
+++ pages/carrinho.js
@@ -12,6 +12,11 @@ const Cart = () => {
   const { carrinho, auth, orders } = state
 
   const [total, setTotal] = useState(0)
+
+  const [address, setAddress] = useState('')
+  const [mobile, setMobile] = useState('')
+
+  const [callback, setCallback] = useState(false)
   const router = useRouter()
 
   useEffect(() => {
@@ -33,7 +38,13 @@ const Cart = () => {
       const updateCart = async () => {
         for (const item of cartLocal) {
           const res = await getData(`product/${item._id}`)
-          const { _id, title, images, price, sold } = res.product
+          const { _id, title, images, price, inStock, sold } = res.product
+          if (inStock > 0) {
+            newArr.push({
+              _id, title, images, price, inStock, sold,
+              quantity: item.quantity > inStock ? 1 : item.quantity
+            })
+          }
         }
 
         dispatch({ type: 'ADD_CART', payload: newArr })
@@ -50,6 +61,18 @@ const Cart = () => {
     let newCart = [];
     for (const item of carrinho) {
       const res = await getData(`product/${item._id}`)
+      if (res.product.inStock - item.quantity >= 0) {
+        newCart.push(item)
+      }
+    }
+
+    if (newCart.length < carrinho.length) {
+      setCallback(!callback)
+      return dispatch({
+        type: 'NOTIFY', payload: {
+          error: 'O produto está fora de estoque ou a quantidade é insuficiente'
+        }
+      })
     }
 
     dispatch({ type: 'NOTIFY', payload: { loading: true } })
